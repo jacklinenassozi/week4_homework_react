8114a7fafccb5b25f922ae32357fe2739dcd8648
katongole <katsisaac50@gmail.com>
2018-09-22 22:21:10 +0200

    component to render names

---
+++ src/components/GitHubRepoName.js
@@ -0,0 +1,45 @@
+import React, {Component}from "react";
+
+class RepoName extends Component{
+    constructor(props){
+        super(props);
+       this.state={url:this.props}
+       this.repoName = this.repoName.bind(this);
+    }
+    repoName(){
+        let url=this.state.url.gitHubData + '/repos';
+
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            // Here's a list of repos!
+            let name = data.map(data => (
+              <li>{data.name}</li>
+            ))
+            this.setState({name}) 
+            console.log(data)
+          });
+  
+      } 
+
+    render(){
+        /* if (this.state.name) {
+            return null;
+          } */
+
+        return(
+            <div>
+            <button onClick={this.repoName}>display names</button>
+            {console.log(this.state)} 'hello'
+            {// Here's a list of repos!
+             /* this.state.data.map(data => (
+              <li>{data.name}</li>
+            )) */ }
+            <ul>{this.state.name}</ul>
+            </div>
+            
+        )
+    }
+}
+
+export default RepoName;
